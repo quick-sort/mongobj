17b0704b49cd6765fc2d5f9c2e596343b0360cb4
quick-sort <quick.sort@outlook.com>
2019-03-28 15:19:50 +0800

    first version

---
+++ README.md
@@ -0,0 +1,60 @@
+MongObj
+=======
+
+MongObj is a Mongo-style api to update javascript object.
+
+# Example
+```js
+import mongobj from 'mongobj'
+let person = {
+    "addresses": [
+    {
+        "country": "China",
+        "city": "Shanghai",
+        "street": [{
+            "road": "Century Avenue",
+            "room": "#201",
+        }, {
+            "road": "People Rd",
+            "room": "#101"
+        }]
+    },
+    {
+        "country": "USA",
+        "city": "Palo Alto"
+    }
+    ]
+}
+mongobj.update(person, {
+    "$set": {
+        "addresses.$[i].city": "Beijing"
+    }
+}, {
+    "arrayFilters": [{"i.country": "China"}]
+})
+
+mongobj.update(person, {
+    "$pull": {
+        "addresses.$[i].city.street": { "road": "Century Avenue" }
+    }
+}, {
+    "arrayFilters": [{"i.country": "China"}]
+})
+
+mongobj.update(person, {
+    "$push": {
+        "addresses": {
+            "country": "France",
+            "city": "Pairs"
+        }
+    }
+})
+mongobj.update(person, {
+    "$unset": {
+        "addresses.$[i].street": ""
+    }
+}, {
+    "arrayFilters": [{"i.country": "China"}]
+})
+console.log(person)
+```
---
+++ index.js
@@ -0,0 +1,118 @@
+const NormalAttr = /^[a-zA-z_].*$/;
+
+function getAttr(obj, attrPath, arrayFilters, level = 0) {
+  let tokens = attrPath.split('.');
+  let parentObj = obj;
+  while (tokens.length > level) {
+    let attr = tokens.splice(0, 1)[0];
+    if (attr.match(NormalAttr)) {
+      if (!(attr in parentObj)) {
+        parentObj[attr] = {};
+      }
+      parentObj = parentObj[attr];
+    } else {
+      let m = attr.match(/^\$\[(.*)\]$/);
+      if (m) {
+        let filter = arrayFilters.find(f => Object.keys(f)[0].startsWith(m[1] + '.'));
+        let key = Object.keys(filter)[0];
+        let filterKey = key.substring(m[1].length + 1, key.length);
+        let filterValue = Object.values(filter)[0];
+        parentObj = parentObj.find(i => getAttr(i, filterKey) === filterValue);
+      } else {
+        parentObj = parentObj[parseInt(attr)];
+      }
+    }
+  }
+  return parentObj;
+}
+
+function objectMatch(obj, filter) {
+  for (let key in filter) {
+    if (key.indexOf('.') >= 0) {
+      console.log('nested attr path is not ready now');
+    }
+    if (key.match(NormalAttr)) {
+      if (typeof filter[key] !== 'object') {
+        return obj[key] === filter[key];
+      } else if (key === '$in') {
+        return filter[key].includes(obj);
+      } else {
+        console.log('filter value is object, not ready now');
+      }
+    } else {
+      console.log('not ready ' + key);
+    }
+  }
+  return false;
+}
+function update(obj, changes, options = {}) {
+  /*
+  changes = {
+    'set' | 'unset' | 'pull' | 'push': {
+    "sections.$[i].title": updateValue,
+    "sections.1.title": updateValue,
+    "context.csv": updateValue
+    }
+  }
+  options = {
+    'arrayFilters': [{'i.title': searchValue}]
+  }
+  */
+  for (let op in changes) {
+    const patches = changes[op];
+    for (let key in patches) {
+      let tokens = key.split('.');
+      let attr = null;
+      let parent = null;
+      switch (op) {
+        case '$unset':
+          parent = getAttr(obj, key, options.arrayFilters, 1);
+          attr = tokens[tokens.length - 1];
+          delete parent[attr];
+          break;
+        case '$pull':
+          parent = getAttr(obj, key, options.arrayFilters);
+          let idx = 0;
+          if (typeof patches[key] === 'object') {
+            while (idx >= 0) {
+              idx = parent.findIndex(i => objectMatch(i, patches[key]));
+              if (idx >= 0) {
+                parent.splice(idx, 1);
+              }
+            }
+
+          } else {
+            while (idx >= 0) {
+              idx = parent.indexOf(patches[key]);
+              if (idx >= 0) {
+                parent.splice(idx, 1);
+              }
+            }
+          }
+
+          break;
+        case '$push':
+          parent = getAttr(obj, key, options.arrayFilters);
+          if (patches[key]['$each']) {
+            patches[key]['$each'].forEach(i => {
+              if ('$position' in patches[key]) {
+                parent.splice(patches[key]['$position'], 0, i);
+              } else {
+                parent.push(i);
+              }
+            });
+          } else {
+            parent.push(patches[key]);
+          }
+          break;
+        default:
+          //set
+          parent = getAttr(obj, key, options.arrayFilters, 1);
+          attr = tokens[tokens.length - 1];
+          parent[attr] = patches[key];
+      }
+    }
+  }
+  return obj;
+}
+export { update };
